2D ME debugged. - The problem was the wrong jacobian and dx/dxi and dy/deta - Ran the simulation using 1D initial conditions(for both directions individually),   and compared with the 1D ME code output, the error was of the order of 10^-8 - Jupyter notebook to compare 2D and 1D ME for 1D initial conditions added and working.

dg_maxwell/params.py
@@ -20,7 +20,7 @@ x_nodes    = af.np_to_af_array(np.array([-1., 1.]))
 N_LGL      = 8
 
 # Number of elements the domain is to be divided into.
-N_Elements = 9
+N_Elements = 10
 
 # The scheme to be used for integration. Values are either
 # 'gauss_quadrature' or 'lobatto_quadrature'
@@ -63,29 +63,29 @@ c_lax_2d = c_x
 
 courant = 0.1
 
-#mesh_file = 'examples/read_and_plot_mesh/mesh/square_10_10.msh'
+mesh_file = 'examples/read_and_plot_mesh/mesh/square_10_10.msh'
 #mesh_file = 'examples/read_and_plot_mesh/mesh/square_contiguous_4_4.msh'
 #mesh_file = 'examples/read_and_plot_mesh/mesh/square_mesh_4_parts.msh'
 #mesh_file = 'examples/read_and_plot_mesh/mesh/particle_in_rectangle.msh'
-mesh_file  = 'examples/read_and_plot_mesh/mesh/square_1_3.msh'
+#mesh_file  = 'examples/read_and_plot_mesh/mesh/square_1_3.msh'
 
-total_time_2d = 2.1
+total_time_2d = 2.01
 
 volume_integrand_scheme_2d = 'Lobatto'
 
-##################################################################
-### Periodic boundary conditions for 10x10 mesh
-##################################################################
-#vertical_boundary_elements_pbc   = np.zeros([10, 2], dtype = np.int64)
-#horizontal_boundary_elements_pbc = np.zeros([10, 2], dtype = np.int64)
+#################################################################
+## Periodic boundary conditions for 10x10 mesh
+#################################################################
+vertical_boundary_elements_pbc   = np.zeros([10, 2], dtype = np.int64)
+horizontal_boundary_elements_pbc = np.zeros([10, 2], dtype = np.int64)
 
-#for idx in np.arange(vertical_boundary_elements_pbc.shape[0]):
-    #vertical_boundary_elements_pbc[idx] = np.array([idx * 10, idx * 10 + 9])
-#print(vertical_boundary_elements_pbc)
+for idx in np.arange(vertical_boundary_elements_pbc.shape[0]):
+    vertical_boundary_elements_pbc[idx] = np.array([idx * 10, idx * 10 + 9])
+print(vertical_boundary_elements_pbc)
 
-#for idx in np.arange(horizontal_boundary_elements_pbc.shape[0]):
-    #horizontal_boundary_elements_pbc[idx] = np.array([idx, idx + 90])
-#print(horizontal_boundary_elements_pbc)
+for idx in np.arange(horizontal_boundary_elements_pbc.shape[0]):
+    horizontal_boundary_elements_pbc[idx] = np.array([idx, idx + 90])
+print(horizontal_boundary_elements_pbc)
 
 
 #################################################################
@@ -182,18 +182,18 @@ volume_integrand_scheme_2d = 'Lobatto'
 #horizontal_boundary_elements_pbc = np.array(horizontal_boundary_elements_pbc)
 
 
-####################################################################
-# Periodic boundary conditions for square 4x4 non-contiguous mesh
-####################################################################
+#####################################################################
+## Periodic boundary conditions for square 4x4 non-contiguous mesh
+#####################################################################
 
-vertical_boundary_elements_pbc = [[ 0,   2]]
+#vertical_boundary_elements_pbc = [[ 0,   2]]
 
-vertical_boundary_elements_pbc = np.array(vertical_boundary_elements_pbc)
+#vertical_boundary_elements_pbc = np.array(vertical_boundary_elements_pbc)
 
 
-horizontal_boundary_elements_pbc = [[ 0,   0],
-                                    [ 1,   1],
-                                    [ 2,   2]]
+#horizontal_boundary_elements_pbc = [[ 0,   0],
+                                    #[ 1,   1],
+                                    #[ 2,   2]]
 
-horizontal_boundary_elements_pbc = np.array(horizontal_boundary_elements_pbc)
+#horizontal_boundary_elements_pbc = np.array(horizontal_boundary_elements_pbc)
 

main.py
@@ -36,12 +36,17 @@ gv = global_variables.advection_variables(params.N_LGL, params.N_quad,
 edge_reorded_mesh = msh_parser.rearrange_element_edges(gv.elements, gv)
 gv.reassign_2d_elements(edge_reorded_mesh)
 
+print('Delta t 2D', gv.delta_t_2d)
 sigma = 0.4
-E_z_init = np.e**(- (gv.x_e_ij**2) / sigma**2)
-B_x_init = af.constant(0., d0 = gv.x_e_ij.shape[0],
-                       d1 = gv.x_e_ij.shape[1],
-                       dtype = af.Dtype.f64)
+E_z_init = np.e**(- (gv.x_e_ij**2 + gv.y_e_ij**2) / sigma**2)
+B_x_init = np.e**(- (gv.y_e_ij**2) / sigma**2)
+#B_x_init = af.constant(0., d0 = gv.x_e_ij.shape[0],
+                       #d1 = gv.x_e_ij.shape[1],
+                       #dtype = af.Dtype.f64)
 B_y_init = np.e**(- (gv.x_e_ij**2) / sigma**2)
+#B_y_init = af.constant(0., d0 = gv.x_e_ij.shape[0],
+                       #d1 = gv.x_e_ij.shape[1],
+                       #dtype = af.Dtype.f64)
 
 u_init = af.join(dim    = 2,
                  first  = E_z_init,

post_processing.py
@@ -63,33 +63,27 @@ def contour_2d(u, index):
     '''
     '''
     color_levels = np.linspace(0, 1.1, 100)
-    u_plot = af.flip(af.moddims(u, params.N_LGL, params.N_LGL, 3, 1), 0)
-    x_plot = af.flip(af.moddims(x_e_ij, params.N_LGL, params.N_LGL, 3, 1), 0)
-    y_plot = af.flip(af.moddims(y_e_ij, params.N_LGL, params.N_LGL, 3, 1), 0)
+    u_plot = af.flip(af.moddims(u, params.N_LGL, params.N_LGL, 10, 10), 0)
+    x_plot = af.flip(af.moddims(x_e_ij, params.N_LGL, params.N_LGL, 10, 10), 0)
+    y_plot = af.flip(af.moddims(y_e_ij, params.N_LGL, params.N_LGL, 10, 10), 0)
 
 
-    x_contour = af.np_to_af_array(np.zeros([params.N_LGL * 3, params.N_LGL * 1]))
-    y_contour = af.np_to_af_array(np.zeros([params.N_LGL * 3, params.N_LGL * 1]))
-    u_contour = af.np_to_af_array(np.zeros([params.N_LGL * 3, params.N_LGL * 1]))
+    x_contour = af.np_to_af_array(np.zeros([params.N_LGL * 10, params.N_LGL * 10]))
+    y_contour = af.np_to_af_array(np.zeros([params.N_LGL * 10, params.N_LGL * 10]))
+    u_contour = af.np_to_af_array(np.zeros([params.N_LGL * 10, params.N_LGL * 10]))
     fig = pl.figure()
-
-    print(x_contour.shape, x_plot.shape)
-    for r in range(3):
-        q = int(r / 3)
-        p = r - q * 1
-
-        print(p * params.N_LGL, ":", params.N_LGL * (p + 1),
-              q * params.N_LGL, ":", params.N_LGL * (q + 1))
-        print(q, p)
-
+    #
+    for r in range(100):
+        p = int(r / 10)
+        q = r - p * 10
         x_contour[p * params.N_LGL:params.N_LGL * (p + 1),\
-                    q * params.N_LGL:params.N_LGL * (q + 1)] = x_plot[:, :, q, p]
+                  q * params.N_LGL:params.N_LGL * (q + 1)] = x_plot[:, :, q, p]
 
         y_contour[p * params.N_LGL:params.N_LGL * (p + 1),\
-                    q * params.N_LGL:params.N_LGL * (q + 1)] = y_plot[:, :, q, p]
+                  q * params.N_LGL:params.N_LGL * (q + 1)] = y_plot[:, :, q, p]
 
         u_contour[p * params.N_LGL:params.N_LGL * (p + 1),\
-                    q * params.N_LGL:params.N_LGL * (q + 1)] = u_plot[:, :, q, p]
+                  q * params.N_LGL:params.N_LGL * (q + 1)] = u_plot[:, :, q, p]
 
     x_contour = np.array(x_contour)
     y_contour = np.array(y_contour)
@@ -97,51 +91,14 @@ def contour_2d(u, index):
     pl.contourf(x_contour, y_contour, u_contour, 200, levels = color_levels, cmap = 'jet')
     pl.gca().set_aspect('equal')
     pl.colorbar()
-    pl.title('Time = %.2f' %(index * 20 * gv.delta_t_2d))
-    fig.savefig('results/2D_Wave_images/%04d' %(index) + '.png', dpi = 300)
+    pl.title('Time = %.2f' %(index * 10 * gv.delta_t_2d))
+    fig.savefig('results/2D_Wave_images/%04d' %(index) + '.png')
     pl.close('all')
     return
 
-
-
-for i in trange(1000):
-    h5py_data = h5py.File('results/2d_hdf5_%02d/dump_timestep_%06d' % (int(params.N_LGL), int(i * 20)) + '.hdf5', 'r')
+for i in trange(0, 1000):
+    h5py_data = h5py.File('results/2d_hdf5_%02d/dump_timestep_%06d' %(int(params.N_LGL), int(10 * i)) + '.hdf5', 'r')
     u_LGL     = af.np_to_af_array(h5py_data['u_i'][:])
-    print(u_LGL[:, :, 0].shape)
-    contour_2d(u_LGL[:, :, 0], i)
-    #if i > 199 :
-        #print(af.mean(af.abs(u_LGL - params.u_e_ij)))
-
-## Creating a folder to store hdf5 files. If it doesn't exist.
-#results_directory = 'results/1D_Wave_images'
-
-#if not os.path.exists(results_directory):
-    #os.makedirs(results_directory)
-
-# The directory where h5py files are stored.
-#h5py_directory = 'results/hdf5_%02d' %int(params.N_LGL)
-#
-#path, dirs, files = os.walk(h5py_directory).__next__()
-#file_count = len(files)
-#print(file_count)
-#
-#
-#for i in range(0, file_count):
-#    fig = pl.figure()
-#    h5py_data = h5py.File('results/hdf5_%02d/dump_timestep_%06d' %(int(params.N_LGL), int(20 * i)) + '.hdf5', 'r')
-#    u_LGL     = (h5py_data['u_i'][:])
-#    pl.plot(params.element_LGL, u_LGL)
-#    pl.xlabel('x')
-#    pl.ylabel('u')
-#    pl.xlim(-1, 1)
-#    pl.ylim(-2, 2)
-#    pl.title('Time = %f' % (i * 20 * params.delta_t))
-#    fig.savefig('results/1D_Wave_images/%04d' %(i) + '.png')
-#    pl.close('all')
-#
-## Creating a movie with the images created.
-#os.system("cd results/1D_Wave_images && ffmpeg -f image2 -i %04d.png -vcodec mpeg4\
-#	  -mbd rd -trellis 2 -cmp 2 -g 300 -pass 1 -r 25 -b 18000000 movie.mp4\
-#	  && rm -f ffmpeg2pass-0.log\
-#          && mv movie.mp4 1D_wave_advection.mp4\
-#          && mv 1D_wave_advection.mp4 ~/git/DG_Maxwell/results && rm *")
+    #print(u_LGL.shape)
+    contour_2d(u_LGL[:, :, 2], i)
+    print(af.min(u_LGL[:, :, 2]))